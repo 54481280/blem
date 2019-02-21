53f19c2a915199042b5ce2e07d15346265cfda44
firstyn <54481280@qq.com>
2019-02-21 15:51:13 +0800

    修改模型名称

--- admin.blem.com/app/Http/Controllers/ShopCategoriesController.php
+++ admin.blem.com/app/Http/Controllers/ShopCategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\shopCategories;
 use Illuminate\Http\Request;
 
 class ShopCategoriesController extends Controller
@@ -14,7 +15,7 @@ class ShopCategoriesController extends Controller
     public function index(Request $request)
     {
         if($request->keyword){
-            
+            $rows = shopCategories::
         }
         return view('Cate.index');
     }
